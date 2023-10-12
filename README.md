b467537138ac4b90f06b410aef19c4f63ae5c5f1
igorliotta <liottaigor@gmail.com>
2023-10-12 17:34:32 +0200

    Fix: ancora da correggere la funzione somma

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
    - Cicliamo tramite FOR reverseWord e facciamo un console.log(reverseWord[i]) per stampare gli indici corrispondenti ad ogni lettera della parola reversed.
 
 - Infine stampare in console SE la parola inserita dall'utente sarà polindroma o no.
+
+# Pari e Dispari
+
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+- Stabiliamo se la somma dei due numeri è pari o dispari usando una funzione.
+  - Creare una funzione per capire se la somma è pari o dispari.
+  - Creare una funzione isSum.
+  - function isSum (somma) {.
+  - SE la somma darà resto 0 ci sarà un return true ALTRIMENTI un return false.
+
+- Facciamo scegliere all'utente tra pari e dispari e gli facciamo inserire un numero che vada da 1 a 5.
+  - Dichiarare una variabile value dove l'utente, tramite un prompt, scriverà pari o dispari.
+  - Facciamo un console.log(value).
+  - Dichiariamo una variabile userNumber dove l'utente, tramite un prompt inserirà un numero da 1 a 5.
+  - Facciamo un console.log(userNumber).
+- Generiamo un numero random da 1 a 5 per il computer tramite una funzione.
+  - Dichiariamo una variabile pcNumber dove tramite la funzione parseInt(Math.random) verrà inserito un numero random da 1 a 5.
+- Sommiamo i due numeri a disposizione.
+  - Dichiariamo una variabile somma che sarà uguale a userNumber + pcNumber.
+- Infine dichiariamo chi ha vinto.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+console.log('PAROLA PALINDROMA')
 function isPalindromeWord (word, reversedWord) {
     // Blocco di codice da eseguire
     if (word === reversedWord) {
@@ -32,3 +33,89 @@ if (isPalindromeWord(wor, revWord)) {
 } else {
     console.log(`${wor} non è una parola palindroma.`);
 }
+
+
+console.log('PARI E DISPARI')
+
+// function isSum (somma) {
+//     // Blocco di codice da eseguire
+//     if (somma % 2 === 0 ) {
+//         return 
+//     } else {
+//         return false
+//     }
+// }
+
+// - Facciamo scegliere all'utente tra pari e dispari e gli facciamo inserire un numero che vada da 1 a 5.
+//   - Dichiarare una variabile value dove l'utente, tramite un prompt, scriverà pari o dispari.
+//   - Facciamo un console.log(value).
+//   - Dichiariamo una variabile userNumber dove l'utente, tramite un prompt inserirà un numero da 1 a 5.
+//   - Facciamo un console.log(userNumber).
+// - Generiamo un numero random da 1 a 5 per il computer tramite una funzione.
+//   - Dichiariamo una variabile pcNumber dove tramite la funzione parseInt(Math.random) verrà inserito un numero random da 1 a 5.
+// - Sommiamo i due numeri a disposizione.
+//   - Dichiariamo una variabile somma che sarà uguale a userNumber + pcNumber.
+// - Infine dichiariamo chi ha vinto.
+
+
+// const value = prompt('Caro utente, scegli tra pari o dispari');
+// console.log(value);
+
+// const somma = userNumber + pcNumber;
+// console.log(somma, 'SOMMA');
+
+// function isEven (somma) {
+//     // Blocco di codice
+//     if (somma % 2 === 0 ) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// if (isSum(somma)) {
+//     console.log(`User hai vinto!!`);
+// } else {
+//     console.log(`User hai perso!!`);
+// }
+
+function isPcNumber (pcNumber) {
+    // Blocco di codice
+   return pcNumber;
+}
+
+function isSumOfNumbers (sum) {
+    
+    return sum
+}
+
+
+const pcNumber = parseInt(Math.random() * 5 + 1);
+console.log(pcNumber, 'PC-NUMBER');
+
+const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+// console.log(userNumber, 'USER-NUMBER');
+
+if (userNumber % 2 === 0) {
+  console.log(userNumber, 'User hai scelto Pari');
+} else {
+  console.log(userNumber, 'User hai scelto Dispari');
+}
+
+const sum =  userNumber + pcNumber;
+
+if (sum % 2 === 0 ) {
+    console.log(sum, 'La somma è pari');
+} else {
+    console.log(sum, 'La somma è dispari');
+}
+
+if (isSumOfNumbers(sum)) {
+    console.log(`User hai vinto!!`);
+} else {
+    console.log(`User hai perso!!`);
+}
+
+
+
+
